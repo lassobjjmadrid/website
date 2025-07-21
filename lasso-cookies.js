661a94b6fa6dcbcb27f5f6c116de11321fb9b3df
lassobjjmadrid <lassobjjmadrid@gmail.com>
2025-07-21 12:52:08 -0300

    Initial Commit

---
+++ lasso-cookies.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+	"palette": {
+	    "popup": {
+		"background": "#383b75"
+	    },
+	    "button": {
+		"background": "#effc15"
+	    }
+	},
+	"theme": "classic",
+	"position": "bottom-right",
+	"content": {
+	    "message": "This website uses cookies."
+	}
+    })
+});
