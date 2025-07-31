363d5795e57eabc5a9c82e5ba6fe953e9e5b7630
MWD <mostwantedduck@gmail.com>
2025-07-31 09:50:10 +0200

    updates

--- lasso-cookies.js
+++ lasso-cookies.js
@@ -1,17 +1,20 @@
-window.addEventListener("load", function(){
-    window.cookieconsent.initialise({
-	"palette": {
-	    "popup": {
-		"background": "#383b75"
-	    },
-	    "button": {
-		"background": "#effc15"
-	    }
-	},
-	"theme": "classic",
-	"position": "bottom-right",
-	"content": {
-	    "message": "This website uses cookies."
-	}
-    })
-});
+  // Mostrar modal solo si no hay preferencia guardada
+  if (!localStorage.getItem('cookiesAccepted')) {
+    $(document).ready(function() {
+      $('#cookieModal').modal('show');
+    });
+  }
+
+  // Botón "Aceptar"
+  $('#acceptCookies').click(function() {
+    localStorage.setItem('cookiesAccepted', 'true');
+    $('#cookieModal').modal('hide');
+    // Aquí puedes cargar scripts de cookies (ej: Google Analytics)
+  });
+
+  // Botón "Rechazar"
+  $('#declineCookies').click(function() {
+    localStorage.setItem('cookiesAccepted', 'false');
+    $('#cookieModal').modal('hide');
+    // Opcional: Deshabilitar cookies de terceros
+  });
